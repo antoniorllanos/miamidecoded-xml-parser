cab01b65b7c954f88a4eae1cb1af0ccc1163b226
Will <fotingkempchee@gmail.com>
2014-05-20 17:48:51 -0400

    parsed first two contents

--- xml-parser.rb
+++ xml-parser.rb
@@ -1,24 +1,23 @@
-class LegalCode
-	def initialize(xml_file)
-		f=File.open(xml_file,"r+")
-		doc=Nokogiri::XML(f)
-		info_tag=doc.xpath("book/bookinfo")
-		puts info_tag.xpath("title")
-	end
-end
 
 require 'nokogiri'
 f=File.open("raw_dump.xml","r+")
+o=File.open("output_file.xml","w+")
+o.write("<?xml version='1.0' encoding='utf-8'?>\n<law>\n<structure>")
 doc=Nokogiri::XML(f)
-content={"section"=>{}}
+part_count=1
 doc.xpath("book/level1").each do |tag|
-	tag.xpath("breadcrumbs/crumb[2]/caption").each do |section|
-		content["section"][section.content.strip]={}
+	tag.xpath("breadcrumbs/crumb[2]/caption").each do |part|
+		depth=1
+		o.write("<unit label='part' identifier=#{part_count.to_s} order_by=#{part_count.to_s} level=#{depth.to_s}>#{part.content}</unit>\n")
+		article_count=1
 		tag.xpath("level2/breadcrumbs/crumb[3]/caption").each do |article|
-			content["section"][section.content.strip][article.content.strip]={}
+			depth=2
+			o.write("<unit label='article' identifier=#{part_count.to_s+'.'+article_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s} level=#{depth.to_s}>#{article.content}</unit>\n")
+			article_count+=1
 		end
 	end
+	part_count+=1
 end
 
-LegalCode.new("raw_dump.xml")
+
 
