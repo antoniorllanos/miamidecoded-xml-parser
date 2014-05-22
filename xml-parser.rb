a0c3b76a3fcb27607c700bbc4631bc78acf6eb5d
Will <fotingkempchee@gmail.com>
2014-05-22 01:18:03 -0400

    new stuff

--- xml-parser.rb
+++ xml-parser.rb
@@ -1,8 +1,8 @@
 
 require 'nokogiri'
-f=File.open("raw_dump.xml","r+")
+f=File.open("test_data.xml","r+")
 o=File.open("output_file.xml","w+")
-o.write("<?xml version='1.0' encoding='utf-8'?>\n<law>\n<structure>")
+o.write("<?xml version='1.0' encoding='utf-8'?>\n")
 doc=Nokogiri::XML(f)
 part_count=1
 doc.xpath("book/level1").each do |tag|
@@ -22,7 +22,15 @@ doc.xpath("book/level1").each do |tag|
 				section_count=1
 				chapter.xpath("section").each do |section|
 					depth=4
-					o.write("<unit label='section' identifier=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s} level=#{depth.to_s}>#{section.xpath('title[1]')[0].content+' '+section.xpath('subtitle[1]')[0].content}</unit>\n</")
+					o.write("<law>\n<structure>")
+					o.write("<section_number=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}></section_number>")
+					o.write("<catch_line>#{section.xpath('subtitle[1]')[0].content}</catch_line>")
+					o.write("<order_by>#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}</order_by>")
+					o.write("<text><section identifier= order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s} level=#{depth.to_s}>#{section.xpath('title[1]')[0].content+' '+section.xpath('subtitle[1]')[0].content}</section></text>")
+					section.xpath("para").each do |para|
+						o.write(para.content)
+					end
+					o.write("</structure></law>")
 					section_count+=1
 				end
 			end
