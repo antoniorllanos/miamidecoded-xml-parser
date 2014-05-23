ee36d674aa239221a0276fd4901d4be37be5e91a
Will <fotingkempchee@gmail.com>
2014-05-23 13:23:50 -0400

    some more progress

--- xml-parser.rb
+++ xml-parser.rb
@@ -8,29 +8,30 @@ part_count=1
 doc.xpath("book/level1").each do |tag|
 	tag.xpath("breadcrumbs/crumb[2]/caption").each do |part|
 		depth=1
-		o.write("<unit label='part' identifier=#{part_count.to_s} order_by=#{part_count.to_s} level=#{depth.to_s}>#{part.content}</unit>\n")
 		article_count=1
 		tag.xpath("level2").each do |article|
-			depth=2
-			o.write("<unit label='article' identifier=#{part_count.to_s+'.'+article_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s} level=#{depth.to_s}>#{article.xpath('breadcrumbs/crumb[3]/caption')[0].content}</unit>\n")
+			depth=2		
 			article_count+=1
 			chapter_count=1
 			article.xpath("level3").each do |chapter|
 				depth=3
-				o.write("<unit label='chapter' identifier=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s} level=#{depth.to_s}>#{chapter.xpath('breadcrumbs/crumb[4]/caption')[0].content}</unit>\n</")
 				chapter_count+=1
 				section_count=1
 				chapter.xpath("section").each do |section|
 					depth=4
-					o.write("<law>\n<structure>")
-					o.write("<section_number=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}></section_number>")
-					o.write("<catch_line>#{section.xpath('subtitle[1]')[0].content}</catch_line>")
-					o.write("<order_by>#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}</order_by>")
-					o.write("<text><section identifier= order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s} level=#{depth.to_s}>#{section.xpath('title[1]')[0].content+' '+section.xpath('subtitle[1]')[0].content}</section></text>")
-					section.xpath("para").each do |para|
-						o.write(para.content)
-					end
-					o.write("</structure></law>")
+					o.write("<law>\n<structure>\n")
+					o.write("<unit label='part' identifier=#{part_count.to_s} order_by=#{part_count.to_s} level=#{depth.to_s}>#{part.content}</unit>\n")
+					o.write("<unit label='article' identifier=#{part_count.to_s+'.'+article_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s} level=#{depth.to_s}>#{article.xpath('breadcrumbs/crumb[3]/caption')[0].content}</unit>\n")
+					o.write("<unit label='chapter' identifier=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s} order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s} level=#{depth.to_s}>#{chapter.xpath('breadcrumbs/crumb[4]/caption')[0].content}</unit>\n")
+					o.write("</structure>\n")
+					o.write("<section_number>#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}</section_number>\n")
+					o.write("<catch_line>#{section.xpath('subtitle[1]')[0].content}</catch_line>\n")
+					o.write("<order_by>#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s}</order_by>\n")
+					o.write("<text>\n<section identifier= order_by=#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s+'.'+section_count.to_s} level=#{depth.to_s}>#{section.xpath('title[1]')[0].content+' '+section.xpath('subtitle[1]')[0].content}</section>\n</text>\n")
+					#section.xpath("para").each do |para|
+						#o.write(para.content)
+					#end
+					o.write("</law>")
 					section_count+=1
 				end
 			end
