303cc7ce1226859563fc274caac0d385298b508a
Will <fotingkempchee@gmail.com>
2014-05-23 18:12:58 -0400

    another push

--- xml-parser.rb
+++ xml-parser.rb
@@ -1,14 +1,11 @@
 
 require 'nokogiri'
 f=File.open("test_data.xml","r+")
-o=File.open("output_file.xml","w+")
-o.write("<?xml version='1.0' encoding='utf-8'?>\n")
 doc=Nokogiri::XML(f)
 part_count=1
 doc.xpath("book/level1").each do |tag|
 	tag.xpath("breadcrumbs/crumb[2]/caption").each do |part|
 		if part_count==3
-
 		else
 			depth=1
 			article_count=1
@@ -21,10 +18,12 @@ doc.xpath("book/level1").each do |tag|
 					chapter.xpath("section").each do |section|
 						depth=4
 						section_name=section.xpath('title[1]')[0].content.strip.split(" ")[1][0..-2]
+						o=File.open("parsed/#{section_name}.xml","w+")
+						o.write("<?xml version='1.0' encoding='utf-8'?>\n")
 						o.write("<law>\n<structure>\n")
-						o.write("<unit label='part' identifier='#{part_count.to_s}' order_by='#{article_count.to_s}' level='#{(depth-3).to_s}'>#{part.content.strip}</unit>\n")
-						o.write("<unit label='article' identifier='#{part_count.to_s+'.'+article_count.to_s}' order_by='#{part_count.to_s+'.'+article_count.to_s}' level='#{(depth-2).to_s}'>#{article.xpath('breadcrumbs/crumb[3]/caption')[0].content.strip}</unit>\n")
-						o.write("<unit label='chapter' identifier='#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s}' order_by='#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s}' level='#{(depth-1).to_s}'>#{chapter.xpath('breadcrumbs/crumb[4]/caption')[0].content.strip}</unit>\n")
+						o.write("<unit label='part' identifier='#{part_count.to_s}' order_by='#{part_count.to_s}' level='#{(depth-3).to_s}'>#{part.content.strip}</unit>\n")
+						o.write("<unit label='chapter' identifier='#{part_count.to_s+'.'+article_count.to_s}' order_by='#{part_count.to_s+'.'+article_count.to_s}' level='#{(depth-2).to_s}'>#{article.xpath('breadcrumbs/crumb[3]/caption')[0].content.strip}</unit>\n")
+						o.write("<unit label='article' identifier='#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s}' order_by='#{part_count.to_s+'.'+article_count.to_s+'.'+chapter_count.to_s}' level='#{(depth-1).to_s}'>#{chapter.xpath('breadcrumbs/crumb[4]/caption')[0].content.strip}</unit>\n")
 						o.write("</structure>\n")
 						o.write("<section_number>#{section_name}</section_number>\n")
 						o.write("<catch_line>#{section.xpath('subtitle[1]')[0].content.strip}</catch_line>\n")
@@ -34,6 +33,7 @@ doc.xpath("book/level1").each do |tag|
 							#o.write(para.content)
 						#end
 						o.write("</law>\n")
+						o.close
 						section_count+=1
 					end
 					chapter_count+=1
